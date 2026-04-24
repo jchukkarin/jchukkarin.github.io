46e315328cb6fc782df783cb0ac3826a280f61fc
jchukkarin <jchukkarin@gmail.com>
2026-04-24 16:35:11 +0700

    edit-base-url

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
-  base: '/portfolio-jchukkarin/',
+  base: '/jchukkarin.github.io/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
